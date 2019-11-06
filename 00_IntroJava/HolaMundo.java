a59d82ca26f0c1d4c9f4f6dcf3fce7fecc1f772e
Jorge <jorgerodicio@gmail.com>
2019-11-06 15:52:07 +0100

    Empezando: HolaMundo.java

---
+++ 00_IntroJava/HolaMundo.java
@@ -0,0 +1,14 @@
+class HolaMundo{
+	
+	public static void main(String [] args){
+		byte unByte = -128;
+		int unEntero = 1000;
+		
+		
+		System.out.println("Hola Ourense! Tipos de datos:");
+		System.out.println("- Byte: " + unByte);
+		System.out.println("- Int: " + unEntero);
+		
+
+	}
+}
