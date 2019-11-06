35a6483d3ac839657901821a8bdf074b85099964
Jorge <jorgerodicio@gmail.com>
2019-11-06 15:54:41 +0100

    - Tipos de datos en Java. - EstructurasControl.java con if y bucles - Test unitarios con JUnit para testear métodos de Ordenamiento.

---
+++ 01_IntroJava/src/com/vn/introjava/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.introjava;
+
+import static com.vn.introjava.DatosBasicos.*;
+import static com.vn.introjava.Ordenamiento.*;
+
+/**
+ * Clase principal del proyecto Ejemplos Java
+ *
+ * @author pc
+ */
+public class Main {
+
+    /**
+     * Función que se ejecuta al arrancar el programa
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        EstructurasControl.ejecutarBucles(); 
+//        EstructurasControl.ejecutarCondiciones();
+//          
+//        mostrarTiposDatos();
+//        DatosBasicos.probarOperadores();
+        
+        //ordenarArray3();
+        
+        
+        ordenarArray3(generarArray(10000));
+    }
+     
+    
+    
+    
+    
+    
+    
+
+}
---
+++ 01_IntroJava/test/com/vn/introjava/tests/TestOrdenamiento.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.introjava.tests;
+
+import com.vn.introjava.Ordenamiento;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static com.vn.introjava.Ordenamiento.*;
+import org.junit.Assert;
+
+/**
+ *
+ * @author pc
+ */
+public class TestOrdenamiento {
+    
+    public TestOrdenamiento() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    //@Test
+    private void probarOrdenamientoSistemaHumano(int [] array, int[] arrayOrdenadoOk) {
+        //int[] array= {5,4,3,2,1};
+        //int[] arrayOrdenadoOk = {1,2,3,4,5};
+        int[] arrayOrdenadoPorFuncion = ordenarArray(array);
+        assertArrayEquals(arrayOrdenadoPorFuncion, arrayOrdenadoOk);
+    }
+       
+    @Test
+    public void probarOrdenamientoSistemaHumano_4Elementos() {
+        int[] array= {4,2,3,1};
+        int[] arrayOrdenadoOk = {1,2,3,4};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_ArrayInverso() {
+        int[] array= {5,4,3,2,1};
+        int[] arrayOrdenadoOk = {1,2,3,4,5};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_ElementosRepetidos() {
+        int[] array= {5,5,4,3,2,2,1};
+        int[] arrayOrdenadoOk = {1,2,2,3,4,5,5};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_MaximoValor() {
+        int[] array= {2,Integer.MAX_VALUE,1,3};
+        int[] arrayOrdenadoOk = {1,2,3,Integer.MAX_VALUE};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_MinimoValor() {
+        int[] array= {2, 1, Integer.MIN_VALUE};
+        int[] arrayOrdenadoOk = {Integer.MIN_VALUE,1,2};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_ArrayVacio() {
+        int[] array= {};
+        int[] arrayOrdenadoOk = {};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_ArrayOrdenado() {
+        int[] array= {1,2,3,4};
+        int[] arrayOrdenadoOk = {1,2,3,4};
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    @Test
+    public void probarOrdenamientoSistemaHumano_ArrayNull() {
+        int[] array= null;
+        int[] arrayOrdenadoOk = null;
+        probarOrdenamientoSistemaHumano(array,arrayOrdenadoOk);
+    }
+    
+    
+    
+}
