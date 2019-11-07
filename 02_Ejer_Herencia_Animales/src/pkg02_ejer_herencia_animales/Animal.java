50b2341600a99dfb43a68dcd7ef61a6bab081501
Julian <juliln_11@hotmail.com>
2019-11-07 17:28:14 +0100

    -Programador B crea clase Percebe y modifica otras clases

--- 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Animal.java
+++ 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Animal.java
@@ -9,6 +9,72 @@ package pkg02_ejer_herencia_animales;
  *
  * @author pc
  */
-public class Animal {
+public abstract class Animal {
     
+    protected Habitat hab;
+    
+private String nombre;
+private int edad;
+private float peso;
+
+    public Animal() {
+    }
+
+    public Animal(Habitat hab, String nombre, int edad) {
+        this.hab = hab;
+        this.nombre = nombre;
+        this.edad = edad;
+
+    }
+
+    
+    
+    public Habitat getHab() {
+        return hab;
+    }
+
+    public void setHab(Habitat hab) {
+        if (hab!=hab.ACUATICO) {
+              this.hab = hab;
+        }
+      
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    
+   
+
+    public void alimentar(String alimento){
+        System.out.println(this.nombre + "mastica y come " + alimento);
+    
+    }
+    
+   public static void mostrar(Animal animal){
+       System.out.println(animal.toString());
+   }
 }
+
+
--- 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Percebe.java
+++ 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Percebe.java
@@ -9,6 +9,19 @@ package pkg02_ejer_herencia_animales;
  *
  * @author pc
  */
-public class Percebe {
+public class Percebe extends Animal{
+
+    public Percebe() {
+    }
+
+    public Percebe(Habitat hab, String nombre, int edad) {
+        super(hab, nombre, edad);
+    }
+    
+    public void alimentar (String alimento){
+        System.out.println(this.getNombre() + " se nutre de " + alimento);
+    
+    }
+    
     
 }
