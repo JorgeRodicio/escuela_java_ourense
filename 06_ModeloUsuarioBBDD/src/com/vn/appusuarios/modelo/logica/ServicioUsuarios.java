43db8770321a3aaeceff69c00b2473f1e26006b3
Jorge <jorgerodicio@gmail.com>
2019-11-27 13:42:01 +0100

    Maven

--- 06_ModeloUsuarioBBDD/src/com/vn/appusuarios/modelo/logica/ServicioUsuarios.java
+++ 06_ModeloUsuarioBBDD/src/com/vn/appusuarios/modelo/logica/ServicioUsuarios.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.appusuarios.modelo;
+package com.vn.appusuarios.modelo.logica;
 
+import com.vn.appusuarios.modelo.Usuario;
+import com.vn.appusuarios.modelo.logica.ChivatoServicios;
+import com.vn.appusuarios.modelo.dao.UsuariosDAO;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
